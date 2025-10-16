001b4503d739eaa1e6bdf5e3be36348d04cc090a
gauciv <vincentaugusto16@gmail.com>
2025-10-16 14:52:02 +0800

    feat: add helper function to get full movie details

--- script.js
+++ script.js
@@ -1,24 +1,68 @@
 // selectors
 const search_input = document.querySelector("#search-input");
 const search_btn = document.querySelector("#search-btn");
+const placeholder = document.querySelector("#placeholder-results");
+const search_results = document.querySelector("#search-results");
 
+// vars
+
+let movies_id = []
+if (movies_id) {
+    placeholder.disabled = true;
+    placeholder.style.display = "none";
+}
+
+
+
+// event listeners
 search_input.addEventListener("input", () => {
     const search_value = search_input.value.trim()
     search_btn.disabled = !search_value;
 });
 
 search_btn.addEventListener("click", async () => {
-    const api = CONFIG.API_KEY;
     const title = search_input.value
-    const url = `${api}&s=${encodeURIComponent(title)}`
+    const url = `${CONFIG.API_KEY}&s=${encodeURIComponent(title)}`
     console.log(url)
     try {
-        const res = fetch(url);
-        const data = (await res).json()
-        console.log(data)
+        const res = await fetch(url);
+        const data = await res.json()
+        if (!res.ok) {
+            throw new Error(`HTTP Error: ${res.status}`)
+        }
+        for (let i=0;i<5;i++) {
+            movies_id.push(data.Search[i].imdbID);
+        }
+        console.log(movies_id)
+        getFullMovieDetails(movies_id)
     } catch (error) {
         console.log(`Caught error: ${error.message}`);
     }
     
 });
 
+// functions
+async function getFullMovieDetails(arr) {
+    let movies = []
+    for (const id of arr) {
+        const url = `${CONFIG.API_KEY}&i=${id}`
+        
+        const res = await fetch(url);
+        const data = await res.json();
+
+        if (!res.ok) {
+            throw new Error(`HTTP Error: ${res.status}`);
+        }
+        
+        movies.push ({
+            title: data.Title,
+            rating: data.imdbRating,
+            runtime: data.Runtime,
+            genre: data.Genre,
+            poster: data.Poster,
+            plot: data.Plot
+        });
+        
+    }
+    console.log(movies)
+}
