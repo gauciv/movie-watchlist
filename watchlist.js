0def5b70f678a1a7d418890da5f4bf6eac423c1f
gauciv <vincentaugusto16@gmail.com>
2025-10-19 00:35:03 +0800

    feat: add logic for watchlist to display movies

--- watchlist.js
+++ watchlist.js
@@ -1,6 +1,42 @@
+const standby = document.querySelector("#placeholder");
+const watchlist_container = document.querySelector("#watchlist-container");
+
 document.querySelectorAll(".search-movies").forEach((btn) => {
     btn.addEventListener("click", () => {
         console.log("button clicked")
         window.location.href = "index.html";
     });
 })
+
+let display = "";
+let storage = JSON.parse(localStorage.getItem("movie"));
+
+if (storage) {
+    standby.disabled = true;
+    standby.style.display = "none";
+}
+
+displayWatchlist(storage, display);
+
+function displayWatchlist(storage, str) {
+    storage.forEach((movie) => {
+    str += `
+            <div class="movie-details">
+                <img class="poster" src="${movie.poster}">
+                <div class="description">
+                    <div class="movie-header">
+                        <p class="header-text">${movie.title}</p>
+                        <p class="rating">⭐ ${movie.rating}</p>
+                    </div>
+                    <div class="movie-stats">
+                        <p class="runtime">${movie.runtime}</p>
+                        <p class="genre">${movie.genre}</p>
+                    </div>
+                    <p class="plot">${movie.plot}</p>
+                </div>
+            </div>
+        `
+    });
+
+    watchlist_container.innerHTML = str;
+}
